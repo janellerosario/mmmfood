6dd45b92dab701642682f81ddaba57fb4f5a189b
Janelle Rosario <janelle.rosario@gmail.com>
2017-02-17 17:18:16 -0500

    added webpack and package.json

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict'
+require('dotenv').config({ silent: true });
+const express     = require('express');
+const logger      = require('morgan');
+const path        = require('path');
+const bodyParser  = require('body-parser');
+const puppyRouter = require('./routes/api/food');
+
+const app         = express();
+const PORT        = process.argv[2] || process.env.port || 3000;
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(bodyParser.json());
+
+app.use('/api/food', foodRouter);
+
+app.listen(PORT, () => console.log('MMMM Im hungry! listening on', PORT));
